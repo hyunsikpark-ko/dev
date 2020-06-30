814dd7305a28b3311783a27fa16442418373a924
hyunsik <hyunsikpark.ko@gmail.com>
2020-06-30 15:27:51 +0900

    define x::container class.

--- c++/x/container.hh
+++ c++/x/container.hh
@@ -5,12 +5,39 @@ namespace x
 {
     class container
     {
-    public:     x::container & operator=(const x::container & o);
-    public:     x::container & operator=(x::container && o) noexcept;
-    public:     container();
-    public:     container(const x::container & o);
-    public:     container(x::container && o) noexcept;
-    public:     virtual ~container();
+    /**
+     * returns the number of elements
+     *
+     * @return  | x::uint64 | the number of elements in the container |
+     */
+    public:     virtual x::uint64 size() const = 0;
+    /**
+     * checks whether the container is empty.
+     *
+     * @return  | x::boolean | true if the container is empty, false otherwise. |
+     */
+    public:     virtual x::boolean empty() const = 0;
+    /**
+     * clear container's all items.
+     */
+    public:     virtual void clear() = 0;
+
+    /**
+     * container class default constructor.
+     */
+    public:     container()
+                {
+                }
+    /**
+     * container class destructor.
+     */
+    public:     virtual ~container()
+                {
+                }
+    public:     container(const x::container & o) = delete;
+    public:     container(x::container && o) noexcept = delete;
+    public:     x::container & operator=(const x::container & o) = delete;
+    public:     x::container & operator=(x::container && o) noexcept = delete;
     };
 }
 
